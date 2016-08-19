ae9740ead736dc859a525b550ea4bfdf001608b9
ponnu <ponnu@qburst.com>
2016-08-19 10:51:22 +0530

    Test configuration file set up

---
+++ tests/region.tests.js
@@ -0,0 +1,9 @@
+describe("Region module", function(){
+	//critical
+	it('ensure there is field to enter region', function() {});
+	it('ensure there is data to be shown in reion dropdown', function() {});
+	it('ensure the value is not empty in the region field', function() {});
+
+	//nice-to-haves
+
+});
